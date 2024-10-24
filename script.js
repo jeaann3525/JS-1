29ed09a85e0b3ea2e2c5e0a71209c3b311c50573
jeaann3525 <163297579+jeaann3525@users.noreply.github.com>
2024-10-24 08:40:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 document.querySelector('mudarTextoBotao').addEventListener('click', function() 
-    const paragrafo = document.querySelector('.descricçâo') ');
+    const paragrafo = document.querySelector('.descricçâo') ';
     paragrafo.textConect = 'texto alterado!;
-})   
+});
+
+document.querySelector(' .destacarBotao') .addEventListener ('click, fuctoin (){
+const titulo=document.querySlector('titulo');
+titulo,classiList.add ('destaque');
+})
+
+                                                             
+                                         
